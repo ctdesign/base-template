2fccb2308cad6a887e69d14fd25cfb4f40ece2fd
Chris Taylor <chris.taylor357@googlemail.com>
2015-01-07 20:24:16 +0000

    updated sass structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,7 @@ if('querySelector' in document
 && 'localStorage' in window
 && 'addEventListener' in window) {
   //if modern browser
+    var html = document.getElementsByTagName('html')[0];
   html.className += ' enhanced'; //add enhanced class
   
   //load in svg class
